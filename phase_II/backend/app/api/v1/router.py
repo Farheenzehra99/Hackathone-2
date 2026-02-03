a3c7bf81a68d5075898bfda6b3c091f60cc8af0d
Farheenzehra99 <farheenzehra481@gmail.com>
2026-02-04 00:47:12 +0500

    Update router.py configuration

--- phase_II/backend/app/api/v1/router.py
+++ phase_II/backend/app/api/v1/router.py
@@ -2,5 +2,5 @@ from fastapi import APIRouter
 from .endpoints import tasks, auth
 
 api_router = APIRouter()
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(auth.router, tags=["auth"])
 api_router.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
